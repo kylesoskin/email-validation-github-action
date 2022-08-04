e9506e47eca424622d055c51fca604c039cc89e7
Kyle Soskin <kyle.soskin@adhocteam.us>
2022-08-04 09:26:17 -0400

    My first action is ready

---
+++ index.js
@@ -0,0 +1,15 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  // `email` input defined in action metadata file
+  const emails = core.getInput('email');
+  console.log(`Hello ${nameToGreet}!`);
+  const time = (new Date()).toTimeString();
+  // core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
