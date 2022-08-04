e9fff392ab8b82b7e6822543d5d7c81a5c0a1b76
Kyle Soskin <kyle.soskin@adhocteam.us>
2022-08-04 10:30:07 -0400

    Splitting

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const github = require('@actions/github');
 
 try {
   // `email` input defined in action metadata file
-  const emails = core.getInput('email');
-  for (const email in emails) {
-    console.log(`Email: ${email}`);
-  }
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const emails = core.getInput('email').split(",").map(item => item.trim());
+  console.log(`Emails: ${emails}`);
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
